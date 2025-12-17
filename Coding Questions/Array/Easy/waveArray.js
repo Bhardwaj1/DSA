41e85e23bc4c9663dd96fe9d475301fda46c1abe
Bhardwaj1 <bhardwajgaurav204@gmail.com>
2025-12-17 12:27:21 +0530

    sortInWave

---
+++ Coding Questions/Array/Easy/waveArray.js
@@ -0,0 +1,29 @@
+// Given an sorted array arr[] of integers. Sort the array into a wave-like array(In Place). In other words, arrange the elements into a sequence such that arr[1] >= arr[2] <= arr[3] >= arr[4] <= arr[5] ..... and so on. If there are multiple solutions, find the lexicographically smallest one.
+
+// Note: The given array is sorted in ascending order, and modify the given array in-place without returning a new array.
+
+// Examples:
+
+// Input: arr[] = [1, 2, 3, 4, 5]
+// Output: [2, 1, 4, 3, 5]
+// Explanation: Array elements after sorting it in the waveform are 2, 1, 4, 3, 5.
+// Input: arr[] = [2, 4, 7, 8, 9, 10]
+// Output: [4, 2, 8, 7, 10, 9]
+// Explanation: Array elements after sorting it in the waveform are 4, 2, 8, 7, 10, 9.
+// Input: arr[] = [1]
+// Output: [1]
+
+function sortInWave(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  for (let i = 0; i < arr.length - 1; i += 2) {
+    [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+  }
+  return arr;
+}
+
+console.log(sortInWave([1, 2, 3, 4, 5]));
+console.log(sortInWave([2, 4, 7, 8, 9, 10]));
+console.log(sortInWave([1]));
+console.log(sortInWave([1, 2]));
